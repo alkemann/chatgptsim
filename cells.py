2f08e66e8af507286b721f35205f5d3aa91cf85d
alkemann <alek.morland@gmail.com>
2023-03-27 21:35:57 +0200

    fixed some bugs

--- cells.py
+++ cells.py
@@ -1,6 +1,7 @@
 
 class CellType:
-    def __init__(self, name, color, nutrient_level):
+    def __init__(self, name, color, nutrient_level, weight):
         self.name = name
         self.color = color
         self.nutrient_level = nutrient_level
+        self.weight = weight
--- environment.py
+++ environment.py
@@ -14,24 +14,24 @@ class Environment:
         self.height = height
         self.cell_size = cell_size
         self.cell_types = self.load_cell_types(config)
+        self.grid = [[None for y in range(self.height // self.cell_size)] for x in range(self.width // self.cell_size)]
         self.generate()
 
     def load_cell_types(self, config):
-        cell_types = {}
+        cell_types = []
         for name, value in config["cell_types"].items():
-            c1,c2,c3, nutrient_level = value.split(",")
+            c1,c2,c3, nutrient_level, weight = value.split(",")
             color = (int(c1), int(c2), int(c3))
             nutrient_level = float(nutrient_level.strip())
-            cell_types[name] = CellType(name, color, nutrient_level)
-
+            cell_types.append(CellType(name, color, nutrient_level, weight))
         return cell_types
 
     def generate(self):
-        # Generate a random grid of cells
+        weights = [cell.weight for cell in self.cell_types]
         for x in range(self.width // self.cell_size):
             for y in range(self.height // self.cell_size):
-                cell_type = random.choices(self.cell_types)
-                self.grid[x][y] = CellType(cell_type)
+                cell_type = np.random.choice(self.cell_types, p=weights)
+                self.grid[x][y] = cell_type
 
     def display(self):
         # Define colors for visualization
@@ -44,7 +44,7 @@ class Environment:
         screen = pygame.display.set_mode((screen_width, screen_height))
         for y in range(self.height):
             for x in range(self.width):
-                if self.environment[y][x] == 0:
+                if self.grid[y][x] == 0:
                     pygame.draw.rect(screen, WHITE, (x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size))
                 else:
                     pygame.draw.rect(screen, BLACK, (x*self.cell_size, y*self.cell_size, self.cell_size, self.cell_size))
